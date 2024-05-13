899b8ee502edea56b94695823e36a3141b9c1260
ImPhatDat <phatdatn2003@gmail.com>
2024-05-13 15:45:34 +0700

    Lab3: update data and initial source

---
+++ Lab_03/src/DataStreaming.py
@@ -0,0 +1,88 @@
+import os
+cwd = os.getcwd()
+
+import findspark
+findspark.init()
+
+# Creating a SparkSession in Python
+from pyspark.sql import SparkSession
+spark = SparkSession.builder\
+    .master("local [*]")\
+    .appName("Spark Streaming")\
+    .getOrCreate()
+
+# keep the size of shuffles small
+spark.conf.set("spark.sql.shuffle.partitions", "2") 
+
+from pyspark.sql.types import *
+
+# define schema for yellow taxi trips
+yellow_taxi_schema = StructType([
+    StructField("type", StringType(), nullable=False),
+    StructField("VendorID", IntegerType()),
+    StructField("tpep_pickup_datetime", TimestampType()),
+    StructField("tpep_dropoff_datetime", TimestampType()),
+    StructField("passenger_count", IntegerType()),
+    StructField("trip_distance", FloatType()),
+    StructField("pickup_longitude", FloatType()),
+    StructField("pickup_latitude", FloatType()),
+    StructField("RatecodeID", FloatType()),
+    StructField("store_and_fwd_flag", StringType()),
+    StructField("dropoff_longitude", FloatType()),
+    StructField("dropoff_latitude", FloatType()),
+    StructField("payment_type", IntegerType()),
+    StructField("fare_amount", FloatType()),
+    StructField("extra", FloatType()),
+    StructField("mta_tax", FloatType()),
+    StructField("tip_amount", FloatType()),
+    StructField("tolls_amount", FloatType()),
+    StructField("improvement_surcharge", FloatType()),
+    StructField("total_amount", FloatType())
+])
+# define schema for green taxi trips
+green_taxi_schema = StructType([
+    StructField("type", StringType(), nullable=False),
+    StructField("VendorID", IntegerType()),
+    StructField("lpep_pickup_datetime", TimestampType()),
+    StructField("Lpep_dropoff_datetime", TimestampType()),
+    StructField("Store_and_fwd_flag", StringType()),
+    StructField("RateCodeID", IntegerType()),
+    StructField("Pickup_longitude", FloatType()),
+    StructField("Pickup_latitude", FloatType()),
+    StructField("Dropoff_longitude", FloatType()),
+    StructField("Dropoff_latitude", FloatType()),
+    StructField("Passenger_count", IntegerType()),
+    StructField("Trip_distance", FloatType()),
+    StructField("Fare_amount", FloatType()),
+    StructField("Extra", FloatType()),
+    StructField("MTA_tax", FloatType()),
+    StructField("Tip_amount", FloatType()),
+    StructField("Tolls_amount", FloatType()),
+    StructField("Ehail_fee", FloatType()),
+    StructField("improvement_surcharge", FloatType()),
+    StructField("Total_amount", FloatType()),
+    StructField("Payment_type", IntegerType()),
+    StructField("Trip_type", IntegerType())
+])
+
+# read the entire data files first
+full_df = spark.readStream\
+    .option("header", False)\
+    .option("inferSchema", True)\
+    .option("maxFilesPerTrigger", 1)\
+    .csv(f"file://{cwd}/data/taxi-data/")
+
+# Filter rows based on the 'type' column
+yellow_df = full_df.where(full_df["_c0"] == "yellow").toDF(schema=yellow_taxi_schema)
+green_df = full_df.where(full_df["_c0"] == "green").toDF(schema=green_taxi_schema)
+
+import pyspark.sql.functions as f
+
+# Same query as staticInputDF
+streamingCountsDF = (                 
+  streamingInputDF
+    .groupBy(
+      streamingInputDF.Action, 
+      window(streamingInputDF.Time, "1 hour"))
+    .count()
+)
